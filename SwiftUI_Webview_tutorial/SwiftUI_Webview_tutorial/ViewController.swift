812ff3c33939b332f49ba71a3972410c548311ce
Hyorin Song <hrsong411@gmail.com>
2021-10-20 21:57:45 +0900

    add .gitignore

---
+++ SwiftUI_Webview_tutorial/SwiftUI_Webview_tutorial/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  SwiftUI_Webview_tutorial
+//
+//  Created by hyorin song on 2021/10/20.
+//
+
+import SwiftUI
+
+struct ViewController: View{
+    var body: some View{
+        //MyWebview(urlToLoad: "https://www.naver.com")
+        
+        NavigationView{
+            HStack{
+                NavigationLink(destination: MyWebview(urlToLoad: "https://www.naver.com").edgesIgnoringSafeArea(.all)){
+                    Text("네이버")
+                        .font(.system(size: 20))
+                        .fontWeight(.bold)
+                        .padding(20)
+                        .background(Color.green)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(20)
+                }
+                NavigationLink(destination: MyWebview(urlToLoad: "https://www.daum.net").edgesIgnoringSafeArea(.all)){
+                    Text("다음")
+                        .font(.system(size: 20))
+                        .fontWeight(.bold)
+                        .padding(20)
+                        .background(Color.orange)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(20)
+                }
+                NavigationLink(destination: MyWebview(urlToLoad: "https://www.google.com").edgesIgnoringSafeArea(.all)){
+                    Text("구글")
+                        .font(.system(size: 20))
+                        .fontWeight(.bold)
+                        .padding(20)
+                        .background(Color.blue)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(20)
+                }
+            }
+        }
+    }
+}
+
+struct ViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewController()
+    }
+}
+
+
+
+
+
